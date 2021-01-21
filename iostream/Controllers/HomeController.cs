d01d050e589c8943585c28d8eb08846244ee7a78
Lenovo <Lenovo@DESKTOP-BNB0PG5>
2021-01-22 01:46:57 +0600

    Добавьте файлы проекта.

---
+++ iostream/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using iostream.Models;
+using iostream.Models.Dbb;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iostream.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDb db;
+        public HomeController(AppDb _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ iostream/Models/Dbb/AppDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iostream.Models.Dbb
+{
+    public class AppDb : DbContext
+    {
+        public DbSet<Request> Requests { get; set; }
+
+        public AppDb(DbContextOptions<AppDb> options) : base(options)
+        {
+            
+        }
+
+    }
+}
